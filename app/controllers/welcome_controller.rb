9dbde8a48f5ae05cb2f7f1c9ae5dc21861f86b1c
Michael Judge <mikelovesrobots@gmail.com>
2009-10-19 21:39:21 -0700

    Added new funnies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,18 @@
 class WelcomeController < ApplicationController
   def index
+    date_o_fire = Time.parse("10/19/09 9:33AM")
+
     @result = "No!"
     @explanation = [
-      "and I'm trying to correct this.", 
-      "and you can help by finding me a Rails job in the Seattle area.",
-      "but this scary face will just keep looking at you until I do.",
+      "... and you can help by finding me a Rails job in the Seattle area.",
+      "... but this scary face will just keep looking at you until I do.",
       "... wait, I got an email, maybe... no.",
-      "and frankly, I'm surprised."
+      "... and frankly, I'm surprised.",
+      "I've been unemployed since Monday.",
+      "... and I hope you lovely bitches know I'm trying to correct this.",
+      "So far I've applied for at least #{AppConfig.jobs_applied} jobs",
+      "I was told I didn't have anything to worry about.",
+      "But that could change if you hire me to work at your Seattle-based office on your Ruby on Rails site."
     ].rand
   end
 end
