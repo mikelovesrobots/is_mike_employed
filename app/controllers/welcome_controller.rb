67e91b8c1a0ec5dc7b070c7c6283332dc8a782f1
Michael Judge <mikelovesrobots@gmail.com>
2009-10-19 21:43:53 -0700

    Removed bitches from one of the blurbs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,12 +5,12 @@ class WelcomeController < ApplicationController
     @result = "No!"
     @explanation = [
       "... and you can help by finding me a Rails job in the Seattle area.",
-      "... but this scary face will just keep looking at you until I do.",
+      "... and this scary face will just keep looking at you until I do.",
       "... wait, I got an email, maybe... no.",
       "... and frankly, I'm surprised.",
       "I've been unemployed since Monday.",
-      "... and I hope you lovely bitches know I'm trying to correct this.",
-      "So far I've applied for at least #{AppConfig.jobs_applied} jobs",
+      "... and I hope you lovely people know I'm trying to correct this.",
+      "So far I've applied for #{AppConfig.jobs_applied} jobs",
       "I was told I didn't have anything to worry about.",
       "But that could change if you hire me to work at your Seattle-based office on your Ruby on Rails site."
     ].rand
